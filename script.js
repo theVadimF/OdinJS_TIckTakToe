87d23ef0c1b3532b8dd2525ad6fe45620f0af164
Vadim Faust <vadim40328@gmail.com>
2023-01-11 18:34:03 +0700

    Grid style

---
+++ script.js
@@ -0,0 +1,7 @@
+const board_wrapper = document.querySelector('#game_board')
+const status_str = document.querySelector('#current_status')
+const reset_btn = document.querySelector('#reset_btn')
+
+function generate_grid() {
+  
+}
